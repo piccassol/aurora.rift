0bb52c4b60f58d8f6057a981c2fbf063f12bbc3d
Noah Michél <noahmichel999@gmail.com>
2025-01-27 14:55:26 -0500

    Update MelissaVisibilityController.cs

--- Melissa/ar-vr-integration/unity/MelissaVisibilityController.cs
+++ Melissa/ar-vr-integration/unity/MelissaVisibilityController.cs
@@ -1,1 +1,22 @@
+using UnityEngine;
 
+public class MelissaVisibilityController : MonoBehaviour
+{
+    public GameObject melissaAvatar; // Assign the avatar in Unity Editor
+    private bool isVisible = true;
+
+    void Update()
+    {
+        // Example: Toggle visibility on "V" key press
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            ToggleVisibility();
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        isVisible = !isVisible;
+        melissaAvatar.SetActive(isVisible);
+    }
+}
