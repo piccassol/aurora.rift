21b83339ca0ee1f0e8c95214fdf222f3085eba35
Noah Michél <noahmichel999@gmail.com>
2025-02-17 13:07:51 -0500

    Update twitter_client.py

--- Melissa/social_media/platforms/twitter_client.py
+++ Melissa/social_media/platforms/twitter_client.py
@@ -1,3 +1,17 @@
 import tweepy
 
-class TwitterClient:
+# Add the necessary functionality here
+def post_tweet(api, message):
+    api.update_status(status=message)
+
+# Example usage
+if __name__ == "__main__":
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+    auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+    # Create API object
+    api = tweepy.API(auth)
+
+    # Post a tweet
+    post_tTweet(api, "Hello, Twitter!")
